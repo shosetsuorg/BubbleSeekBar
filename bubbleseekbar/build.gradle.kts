dd6c629b40354b258f6351e4ce8354f347b21a4b
doomsdayrs <doomsdayrs@gmail.com>
2021-07-07 13:43:03 -0400

    Update to jdk8

--- bubbleseekbar/build.gradle.kts
+++ bubbleseekbar/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
 
 	implementation("androidx.appcompat:appcompat:1.3.0")
 	implementation("androidx.core:core-ktx:1.6.0")
-	implementation(kotlin("stdlib-jdk7", "1.5.0"))
+	implementation(kotlin("stdlib-jdk8", "1.5.0"))
 }
 
 afterEvaluate {
